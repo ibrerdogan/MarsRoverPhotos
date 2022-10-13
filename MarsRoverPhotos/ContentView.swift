c9465740ab1fb78e05838221ef629bef4d62092d
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2022-10-13 22:15:52 +0300

    Initial Commit

---
+++ MarsRoverPhotos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarsRoverPhotos
+//
+//  Created by İbrahim Erdogan on 13.10.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarsRoverPhotos/MarsRoverPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsRoverPhotosApp.swift
+//  MarsRoverPhotos
+//
+//  Created by İbrahim Erdogan on 13.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct MarsRoverPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
